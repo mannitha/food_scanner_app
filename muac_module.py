b7490671d98728aebf637673c074c23fb19ea464
mannitha <mannithamanu@gmail.com>
2025-05-09 15:20:12 +0530

    Update muac_module.py

--- muac_module.py
+++ muac_module.py
@@ -6,44 +6,7 @@ import mediapipe as mp
 
 def muac():
         
-    # --- Custom CSS ---
-    st.markdown("""
-    <style>
-        /* Green upload button */
-        div.stButton > button:first-child {
-            background-color: #69AE43;
-            color: white;
-            border: none;
-            padding: 10px 24px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            cursor: pointer;
-            border-radius: 8px;
-        }
 
-        div.stButton > button:first-child:hover {
-            background-color: #45a049;
-        }
-
-        .nutrition-header {
-            font-size: 18px !important;
-            margin-bottom: 10px !important;
-        }
-
-        /* Extra button styles */
-        .green-button {
-            background-color: #69ae43 !important;
-        }
-
-        .blue-button {
-            background-color: #1889cb !important;
-        }
-
-    </style>
-    """, unsafe_allow_html=True)
 
     # --- Calibration Factors ---
     CALIBRATION_FACTORS = {
