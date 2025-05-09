6614b7a694f3d58c270063617067c10feb628c0a
mannitha <mannithamanu@gmail.com>
2025-05-09 16:53:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def calculate_malnutrition_status(bmi, arm):
 # Flow pages
 def select_flow_step():
     st.title("MalnoCare")
-    st.info("Scan Track Grow")
+    st.write("Scan Track Grow")
     col1, col2 = st.columns(2)
     with col1: 
         if st.button("👶 Physical Attributres"): st.session_state.page = "nutrition_choices"
